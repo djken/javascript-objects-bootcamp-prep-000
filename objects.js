c5eeafd22c9da34db56de83df25e88c6fc18144b
Jean-Kenel Dessources <jkenel_23@yahoo.fr>
2019-02-18 02:57:27 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -1,5 +1,12 @@
-var meals = {
-  breakfast: "oatmeal",
-  lunch: "turkey sandwich",
-  dinner: "steak and potatoes"
-};
+var playlist = new Object({keys: '0'});
+
+function updatePlaylist(playlist, artistName, songTitle) {
+  playlist[artistName] = songTitle
+  return playlist
+}
+
+function removeFromPlaylist(playlist, artistName) {
+  delete playlist[artistName]
+  return playlist
+}
+     
